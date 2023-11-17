7e18714e57d3bc33025b5c6f7e9e16facab111d5
kikefdezl <enriquelagui@outlook.com>
2023-11-17 08:53:14 +0100

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-
+#
+# Every 6 hours, there's a 50% chance a commit is done
 # In my crontab:
-# 0 8 * * * cd ~/commitment && ./commit.sh
+# 0 */6 * * * cd ~/commitment && ./commit.sh
 
-N_COMMITS=$(($RANDOM % 4))
-CURRENT_DATE_TIME=$(date)
 
 git pull
 
-for (( i=0; i<=$N_COMMITS; i++ ))
-do
-  COMMIT_MSG="Commit $i at: $CURRENT_DATE_TIME"
+CURRENT_DATE_TIME=$(date)
+
+if [ $((RANDOM % 10 + 1)) -le 5 ]; then
+  COMMIT_MSG="Commit at: $CURRENT_DATE_TIME"
   git commit --allow-empty -m "$COMMIT_MSG"
   echo $COMMIT_MSG
-done
+fi
 
 git push
 
